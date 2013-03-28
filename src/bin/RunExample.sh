9710289da0d255dd816d2731b7e1b9cdd3ce8721
David Warne <david.warne@qut.edu.au>
2013-03-28 22:06:47 +1000

    Don't know how that nested dirctory happened... whoops deleted.

--- src/bin/RunExample.sh
+++
@@ -1,1 +0,0 @@
-./VecVis -f ../SampleData/TurbulentFlow.flw  -d 0.075 -a 0.05 -s 7500 -an ../SampleData/TurbulentFlow.ans -dim 1280 720
--- src/bin/mkmovie.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "encoding... this may take a while"
-mencoder "mf://*.bmp" -mf fps=60:type=bmp -o test.avi -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=22000000 
